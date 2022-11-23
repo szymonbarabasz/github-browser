3ea58d56a77baad7ce723006f13936deb80d6a56
szymonbarabasz <79063933+szymonbarabasz@users.noreply.github.com>
2022-11-23 19:38:54 +0100

    added sorting

--- src/components/Table/ResultTable.tsx
+++ src/components/Table/ResultTable.tsx
@@ -11,19 +11,64 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import LinkIcon from "@mui/icons-material/Link";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import AvatarModal from "./AvatarModal";
 import { useStatesContext } from "../StatesContext";
 
+type Order = "asc" | "desc";
+
+interface Data {
+  name: string;
+  html_url: string;
+  repository: {
+    description: string | null;
+    owner: { login: string; avatar_url: string };
+  };
+}
+
+interface TableDataProps {
+  responseData: ResponseDataTypes;
+  order: Order;
+  orderBy: string;
+}
+
 function TableData({
   responseData,
-}: {
-  responseData: ResponseDataTypes;
-}): JSX.Element {
+  order,
+  orderBy,
+}: TableDataProps): JSX.Element {
   const [isOpen, setIsOpen] = useState(false);
   const handleOpenModal = () => setIsOpen(!isOpen);
 
-  const responseDataArr = responseData.items.map(
-    ({ name, html_url, repository }, index) => {
+  function sortFunction(a: Data, b: Data) {
+    let comparedDataA: string | null | undefined = "";
+    let comparedDataB: string | null | undefined = "";
+
+    if (orderBy === "name") {
+      comparedDataA = a.name.toLowerCase();
+      comparedDataB = b.name.toLowerCase();
+    } else if (orderBy === "description") {
+      comparedDataA = a.repository.description?.toLowerCase();
+      comparedDataB = b.repository.description?.toLowerCase();
+    } else if (orderBy === "user") {
+      comparedDataA = a.repository.owner.login.toLowerCase();
+      comparedDataB = b.repository.owner.login.toLowerCase();
+    }
+
+    if (comparedDataA && comparedDataB) {
+      if (comparedDataB < comparedDataA) {
+        return order === "desc" ? -1 : 1;
+      }
+      if (comparedDataB > comparedDataA) {
+        return order === "desc" ? 1 : -1;
+      }
+    }
+    return 0;
+  }
+
+  const responseDataArr = responseData.items
+    .sort(sortFunction)
+    .map(({ name, html_url, repository }, index) => {
       return (
         <TableRow key={name + index}>
           <TableCell>
@@ -46,8 +91,7 @@ function TableData({
           </TableCell>
         </TableRow>
       );
-    }
-  );
+    });
 
   return <>{responseDataArr}</>;
 }
@@ -57,11 +101,23 @@ interface ResultTablePropsTypes {
   requestHandle: (page: number, rowsPerPage: number) => void;
 }
 
+interface headCellTypes {
+  id: string;
+  label: string;
+}
+
 export default function ResultTable({
   responseData,
   requestHandle,
 }: ResultTablePropsTypes): JSX.Element {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<string>("name");
   const { page, setPage, rowsPerPage, setRowsPerPage } = useStatesContext();
+  const headCell: readonly headCellTypes[] = [
+    { id: "name", label: "Nazwa pliku i odsyłacz do pliku" },
+    { id: "description", label: "Opis repozytorium" },
+    { id: "user", label: "Użytkownik i avatar" },
+  ];
 
   const handleChangePage = (_event: unknown, newPage: number) => {
     setPage(newPage);
@@ -76,6 +132,33 @@ export default function ResultTable({
     requestHandle(page, +event.target.value);
   };
 
+  const handleRequestSort = (
+    _event: React.MouseEvent<unknown>,
+    property: string
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const headCellsMapped = headCell.map((el) => {
+    return (
+      <TableCell
+        align={el.id !== "name" ? "center" : "inherit"}
+        key={el.id}
+        sortDirection={orderBy === el.id ? order : false}
+      >
+        <TableSortLabel
+          active={orderBy === el.id}
+          direction={orderBy === el.id ? order : "asc"}
+          onClick={(e) => handleRequestSort(e, el.id)}
+        >
+          {el.label}
+        </TableSortLabel>
+      </TableCell>
+    );
+  });
+
   return (
     <Paper
       sx={{
@@ -87,15 +170,15 @@ export default function ResultTable({
       <TableContainer>
         <Table sx={{ minWidth: 650 }}>
           <TableHead>
-            <TableRow>
-              <TableCell>Nazwa pliku i odsyłacz do pliku</TableCell>
-              <TableCell align="center">Opis repozytorium</TableCell>
-              <TableCell align="center">Użytkownik i avatar</TableCell>
-            </TableRow>
+            <TableRow>{headCellsMapped}</TableRow>
           </TableHead>
           <TableBody>
             {responseData.total_count ? (
-              <TableData responseData={responseData} />
+              <TableData
+                responseData={responseData}
+                order={order}
+                orderBy={orderBy}
+              />
             ) : (
               <></>
             )}
