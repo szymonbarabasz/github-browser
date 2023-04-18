4bfd3be326f5b2c6938612ad6f3d70100b35c0f9
szymonbarabasz <szymon.barabasz95@gmail.com>
2023-04-18 21:13:48 +0200

    auth fix 1.0

--- src/services/axiosConfig.tsx
+++ src/services/axiosConfig.tsx
@@ -5,8 +5,7 @@ const api = axios.create({
   headers: {
     Accept: "application/vnd.github.v3+json",
     "X-GitHub-Api-Version": "2022-11-28",
-    Authorization:
-      "token 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",
+    Authorization: "Token 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",
   },
 });
 
